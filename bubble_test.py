4939426d2cc58f45625cea26ab9c31d6496a0581
valorant-dhruv <78591597+valorant-dhruv@users.noreply.github.com>
2024-08-26 22:51:23 +0000

    added testcases using pytest

---
+++ bubble_test.py
@@ -0,0 +1,12 @@
+from main import *
+
+def testsort():
+    tosort = [5,4,2,3,1,4,6]
+    expectedsort=[1, 2, 3, 4, 4, 5, 6]
+    assert bubblesort(tosort)==expectedsort
+
+def testsort2():
+    tosort2=[10,14,12,16,100]
+    expectedsort2=[12,10,14,16,100]
+    assert bubblesort(tosort2)==expectedsort2
+
--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 #Bubble sort
-arr = [5,4,2,3,1,4,6]
-
-for i in range(len(arr)):
-    for j in range(i+1,len(arr)):
-        if(arr[i]>arr[j]):
-            swap(arr,i,j)
 
 def swap(arr,first,second):
     temp=arr[first]
     arr[first]=arr[second]
     arr[second]=temp
 
-print(arr)
+def bubblesort(arr):
+    for i in range(len(arr)):
+        for j in range(i+1,len(arr)):
+            if(arr[i]>arr[j]):
+                swap(arr,i,j)
+    return arr
